125cabab41ee1d15b36e78ea7ebb635788601471
n0zzy <ffnn83@yandex.ru>
2022-01-24 17:46:12 +0500

    update 0.1.1

--- benchmark/jfquery.js
+++ benchmark/jfquery.js
@@ -1,66 +1,96 @@
 const $$ = new function () {
+    'use strict';
     this.elem = null;
-    this.query = (selector)=> {
-        this.elem = document.querySelector(selector);
+    this.query = (elem)=> {
+        this.elem = [];
+        this.elem.push("string" == typeof elem
+            ? document.querySelector(elem)
+            : elem
+        );
+        return this;
+    };
+    this.queryAll = (selector)=> {
+        this.elem = document.querySelectorAll(selector);
         return this;
     };
     this.ready = (fn)=> {
-        if(this.elem != null) {
+        if(this.elem[0] !== null && this.elem[0] !== undefined) {
             fn(this, this.elem);
             return {error: false};
         }
         return {error: true};
     };
     this.css = (styles)=> {
-        for(let s in styles){
-            this.elem.style[s] = styles[s];
-        }
+        this.elem.forEach((o)=>{
+            for(let s in styles){
+                o.style[s] = styles[s];
+            }
+        });
         return this;
     };
     this.append = (content)=> {
-        switch(typeof content){
-            case "string": this.elem.innerHTML += content; break;
-            case "object": this.elem.appendChild(content); break;
-        }
+        this.elem.forEach((o)=>{
+            switch(typeof content){
+                case "string": o.innerHTML += content; break;
+                case "object": o.appendChild(content); break;
+            }
+        });
         return this;
     };
     this.prepend = (content)=> {
-        switch(typeof content){
-            case "string": this.elem.innerHTML = content + this.elem.innerHTML; break;
-            case "object": this.elem.prepend(content); break;
-        }
+        this.elem.forEach((o)=>{
+            switch(typeof content){
+                case "string": o.innerHTML = content + this.elem.innerHTML; break;
+                case "object": o.prepend(content); break;
+            }
+        });
         return this;
     };
     this.remove = ()=> {
-        this.elem.remove();
-        this.elem = null;
+        this.elem.forEach((o)=>{
+            o.remove();
+        });
     };
     this.clear = ()=> {
-        this.elem = null;
+        this.elem = [];
     };
     this.attr = (name, value = "")=> {
-        this.elem.setAttribute(name, value);
+        this.elem.forEach((o)=>{
+            o.setAttribute(name, value);
+        });
         return this;
     };
     this.addClass = ()=> {
-        this.elem.classList.add(selector);
+        this.elem.forEach((o)=>{
+            o.classList.add(selector);
+        });
         return this;
     };
     this.removeClass = (selector)=> {
-        this.elem.classList.remove(selector);
+        this.elem.forEach((o)=>{
+            o.classList.remove(selector);
+        });
         return this;
     };
     this.click = (click)=> {
-        this.elem.onclick = click;
+        this.elem.forEach((o)=>{
+            o.onclick = click;
+        });
     };
     this.text = (content)=> {
-        this.elem.innerText = content;
+        this.elem.forEach((o)=>{
+            o.innerText = content;
+        });
     };
-    this.html = (content)=> {
-        this.elem.innerHTML = content;
+    this.html = (content, append = false)=> {
+        this.elem.forEach((o)=>{
+            o.innerText = content;
+            !!append ? o.innerHTML += content : o.innerHTML = content;
+        });
     };
 
     this.module = {};
+
     this.extends = (name, fn)=> {
         this.module[name] = ()=> {
             fn($$.elem);
